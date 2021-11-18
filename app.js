6e2176721ecf5f5f8b9eae13cea69d7913ead3f5
glebson1988 <glebson1988@yandex.ru>
2021-11-18 15:33:05 +0200

    distinguish tasks into todo and completed

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    tasks: [
-      { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false },
-      { id: 2, name: 'Todo 2', description: 'This is another todo', completed: true },
-      { id: 3, name: 'Three', description: 'This is a compplete todo', completed: true },
-      { id: 4, name: 'Four', description: 'This is another complete todo', completed: true }
-    ],
-    message: 'Hello world!'
-  }
+    el: '#app',
+    data: {
+        tasks: [
+            {id: 1, name: 'Todo 1', description: 'This is a todo', completed: false},
+            {id: 2, name: 'Todo 2', description: 'This is another todo', completed: true},
+            {id: 3, name: 'Three', description: 'This is a compplete todo', completed: true},
+            {id: 4, name: 'Four', description: 'This is another complete todo', completed: true}
+        ],
+        message: 'Hello world!'
+    },
+    computed: {
+        completedTasks: function () {
+            return this.tasks.filter(item => item.completed == true);
+        },
+        todoTasks: function () {
+            return this.tasks.filter(item => item.completed == false);
+        }
+    },
 })
